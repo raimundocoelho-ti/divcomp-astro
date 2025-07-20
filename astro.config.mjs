ebf8ae78b0e162283a87ed4b53c244d8f3303586
Raimundo Coelho TI <raimundocoelho.ti@gmail.com>
2025-07-19 22:55:34 -0300

    header e Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://divcomp.com.br",
   integrations: [mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
